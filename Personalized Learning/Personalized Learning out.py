c75b861c0ca290fd2ce2b293c287281c97e0bd0f
abishakmanikandan <132939128+abishakmanikandan@users.noreply.github.com>
2024-11-10 11:43:59 +0530

    Add files via upload

---
+++ Personalized Learning/Personalized Learning out.py
@@ -0,0 +1,7 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============================================= RESTART: C:\Users\Abishak M\Desktop\Personalized Learning.py =============================================
+{'student': 'Alice', 'recommended_content': ['content_1', 'content_2']}
+{'student': 'Bob', 'recommended_content': ['content_3', 'content_4']}
+{'student': 'Charlie', 'recommended_content': ['content_5', 'content_1']}
